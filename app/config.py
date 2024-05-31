64a6de498cd3f17dc496d95222846dbf1225e325
EhsanTgv <EhsanTgv@gmail.com>
2024-05-31 10:08:08 +0300

    Implement Model

--- app/config.py
+++ app/config.py
@@ -5,5 +5,5 @@ from sqlalchemy.ext.declarative import declarative_base
 DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/python_db"
 
 engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autflush = False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autflush=False, bind=engine)
 Base = declarative_base()
